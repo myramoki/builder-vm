0bce5299462e1041052c6e665f61fdcbc533a2fc
Doug McClure <dsm.mcclure@gmail.com>
2025-05-23 00:06:22 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# fedora-vm
+
+Simple scripts for creating generic fedora VMs
+
+##
+
+Simple command to fetch
+
+```bash
+sudo sh -c "$(curl -sL https://tinyurl.com/n7kreyu8)"
+```
+
+Long form
+
+```bash
+sudo sh -c "$(curl -sL https://raw.githubusercontent.com/myramoki/fedora-vm/java24/setup.sh)"
+```
---
+++ setup.sh
@@ -0,0 +1,82 @@
+GITDIR="https://raw.githubusercontent.com/myramoki/builder-vm/main"
+export GITDIR
+
+SUDO_USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+export SUDO_USER_HOME
+
+DEFAULT_JAVA_VERSION=21
+DEFAULT_GRADLE_VERSION=8.14
+DEFAULT_TOMCAT_VERSION=9
+
+export DEFAULT_JAVA_VERSION
+export DEFAULT_GRADLE_VERSION
+export DEFAULT_TOMCAT_VERSION
+
+echo "
+##
+## Choose which setup you want to run:
+##
+##   S - Starter setup [only]
+##   J - Java [only]
+##   G - Github [only]
+##
+##   a - Automation [starter, java, github, automation]
+##   S - Automation [only]
+##
+"
+
+read -p "?? Select setup type: " respType
+
+_basic() {
+	printf "%s\n" $GITDIR/001-software.sh \
+		$GITDIR/002-network.sh \
+		$GITDIR/003-cifs.sh \
+		$GITDIR/090-user.sh \
+		$GITDIR/099-misc.sh
+}
+
+_java() {
+	printf "%s\n" $GITDIR/101-java.sh
+}
+
+_github() {
+	printf "%s\n" $GITDIR/201-github-ssh.sh
+}
+
+_automation() {
+	printf "%s\n" $GITDIR/501-sftp.sh \
+		$GITDIR/502-build-automation.sh
+}
+
+if [ -n "$respType" ]; then
+	case $respType in
+	S)	echo "# Processing Starter [only]"
+		sh -c "$(curl $(_basic))"
+		;;
+
+	J)	echo "# Processing Java [only]"
+		sh -c "$(curl $(_java))"
+		;;
+
+	G)	echo "# Processing Github setup [only]"
+		sh -c "$(curl $(_github))"
+		;;
+
+	a)	echo "# Automation"
+		sh -c "$(curl $(_basic) $(_java) $(_github) $(_automation))"
+		;;
+
+	A)	echo "# Automation [only]"
+		sh -c "$(curl $(_automation))"
+		;;
+	esac
+fi
+
+if [ -e /tmp/dofinal ]; then
+	sh -c "$(cat /tmp/dofinal)"
+fi
+
+if [ -e /tmp/doreboot ]; then
+    read -t 5 -p "Press ENTER before reboot"
+	reboot
+fi
