ddcf848388bd1a003875a9efe20e60630fcd169c
Doug McClure <dsm.mcclure@gmail.com>
2025-05-27 19:33:14 -0400

    remove at command from installs

--- 001-software.sh
+++ 001-software.sh
@@ -2,7 +2,7 @@ printf "\n#### BEGIN CONFIG : Software\n\n"
 
 dnf -y -q upgrade
 dnf -y -q copr enable lihaohong/yazi
-dnf -y -q install vim stow git at yazi
+dnf -y -q install vim stow git yazi
 
 # https://discussion.fedoraproject.org/t/vim-default-editor-in-coreos/71356/4
 dnf -y swap nano-default-editor vim-default-editor --allowerasing
